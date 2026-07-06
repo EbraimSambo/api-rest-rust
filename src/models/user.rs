3444981a42884ac87b7f01b18d86c78c9416ae16
ebraimsambo <ebraimsambo@gmail.com>
2026-07-06 14:08:47 +0100

    Add User model, establish database connection, and create mod.rs files for repositories and services

--- src/models/user.rs
+++ src/models/user.rs
@@ -1,3 +1,9 @@
+use diesel::prelude::*;
+use chrono::{DateTime, Utc};
+
+#[derive(Queryable, Selectable)]
+#[diesel(table_name = crate::schema::users)]
+#[diesel(check_for_backend(diesel::pg::Pg))]
 pub struct User{
     id: String,
     name: String,
